69fd3cf4c33b4aeee3e1f3650aca84131aeb970c
ritathesaver <ritathesaver@gmail.com>
2020-12-28 00:42:15 +0300

    fixed coming soon

--- src/screens/cart/ComingSoon.tsx
+++ src/screens/cart/ComingSoon.tsx
@@ -1,13 +1,20 @@
+/* eslint-disable react-native/no-inline-styles */
 import React, {FunctionComponent} from 'react'
-import { Text, View } from 'react-native'
+import {Text, View} from 'react-native'
 
 export const CartDetails: FunctionComponent = () => {
 
   //console.log(pictures)
 
   return (
-        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-          <Text>Coming soon...</Text>
-       </View>
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#202122',
+      }}>
+      <Text style={{color: '#f7f7f7', fontSize: 20}}>COMING SOON...</Text>
+    </View>
   )
 }
